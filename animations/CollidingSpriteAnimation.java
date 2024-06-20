8d062274161bbdbdf409e925df95bcb16e7853f0
gjwehnes <gjwehnes@E829WL170006.org.cbe.ab.ca>
2024-06-19 18:32:21 -0600

    transition function for universe implemented

--- animations/CollidingSpriteAnimation.java
+++ animations/CollidingSpriteAnimation.java
@@ -1,25 +1,46 @@
 
 public class CollidingSpriteAnimation implements Animation {
 
-	private int universeCount = 0;
-	private Universe current = null;
+	private Universe current = new CollidingSpriteUniverse();
+	private boolean universeSwitched = false;
+	private boolean animationComplete = false;
 	
-	public Universe getNextUniverse() {
+	public Universe switchUniverse(Object event) {
 
-		universeCount++;
+		universeSwitched = true;
+		//there is only the initial universe; when a switch is called for, set current to null;
+		animationComplete = true;
 		
-		if (universeCount == 1) {
-			this.current = new CollidingSpriteUniverse();
-		}
-		else {
-			this.current = null;
-		}
-		return this.current;
+		return current;
 
 	}
 
 	public Universe getCurrentUniverse() {
-		return this.current;
+		return current;
+	}
+	
+	@Override
+	public boolean getUniverseSwitched() {
+		return universeSwitched;
+	}
+
+	@Override
+	public void acknowledgeUniverseSwitched() {
+		this.universeSwitched = false;		
+	}
+
+	@Override
+	public boolean isComplete() {
+		return animationComplete || current.isComplete();
+	}
+
+	@Override
+	public void setComplete(boolean complete) {
+		this.animationComplete = true;		
+	}
+
+	@Override
+	public void update(KeyboardInput keyboard, long actual_delta_time) {
 	}
 	
 }
